79f2984d6a4b968e3f72e4db47bea868d75f63b6
Reicela <reicelapetersone@gmail.com>
2022-11-08 10:10:19 +0200

    formating

--- PersonsApp/PersonsWeb/wwwroot/js/site.js
+++ PersonsApp/PersonsWeb/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//$(PersonPartialView).ready(function () {
+//    $('.btn-update').prop('disabled', true);
+//});
+
+//$('.optclick').click(function () {
+//    $('.btn-add').prop('disabled', false);
+//});
