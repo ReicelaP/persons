f43a79358866b544d07d0de03c06fe849115f47a
Reicela <reicelapetersone@gmail.com>
2022-11-10 16:34:26 +0200

    formating changes

--- PersonsApp/PersonsWeb/Models/AddPersonViewModel.cs
+++ PersonsApp/PersonsWeb/Models/AddPersonViewModel.cs
@@ -10,19 +10,14 @@ namespace PersonsWeb.Models
             Address = new List<string>();
         }
 
-        [Required]
         public string FirstName { get; set; }
 
-        [Required]
         public string LastName { get; set; }
 
-        [Required]
         public DateTime BirthDate { get; set; }
 
-        [Required]
         public IList<string> PhoneNumber { get; set; }
 
-        [Required]
         public IList<string> Address { get; set; }
 
         public string Primary { get; set; }
--- PersonsApp/PersonsWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PersonsWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
